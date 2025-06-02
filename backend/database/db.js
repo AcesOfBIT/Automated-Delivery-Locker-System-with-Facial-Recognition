78302f6b9af10312c6f2832e4063eab4bd16ea30
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-02 12:17:41 +0530

    DB connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,1 +1,14 @@
-import mongoose from "mongoose";
+import mangoose from 'mongoose'
+
+const connectdb = async() => {
+    try{
+        await mangoose.connect(process.env.MONGO_URI, {
+            dbName: "Delivery locker"
+        });
+        console.log("Database Connected");
+    } catch(error){
+        console.log(error);
+    }
+};
+
+export default connectdb;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectdb from './database/db.js'
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Server working fine");
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+    connectdb();
+})
