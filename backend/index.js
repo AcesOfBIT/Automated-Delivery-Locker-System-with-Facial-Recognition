13430682c414caa08ebe839597d86ddb5fbf620c
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-02 14:27:55 +0530

    created controllers and routers

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectdb from './database/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectdb from "./database/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import courierRoutes from "./routes/courierRoutes.js";
+import lockerRoutes from "./routes/lockerRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT;
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/user", userRoutes);
+app.use("/api/courier", courierRoutes);
+app.use("/api/locker", lockerRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Server working fine");
-})
+  res.send("Server working fine");
+});
 
-app.listen(port, () => {
-    console.log("Server is running on port: " + port);
-    connectdb();
-})
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + port);
+  connectdb();
+});
