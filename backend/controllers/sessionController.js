2d6876baf23319f95c8e3da3b10f914df251b06e
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-05 22:59:52 +0530

    get active sessions

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -3,7 +3,7 @@ import TryCatch from "../utils/TryCatch.js";
 
 export const getAllSessions = TryCatch(async (req, res) => {
   const sessions = await Session.find().populate("userId", "name email role");
-  res.status(200).json(sessions);
+  res.status(200).json({ sessions });
 });
 
 export const getUserSessions = TryCatch(async (req, res) => {
@@ -11,12 +11,32 @@ export const getUserSessions = TryCatch(async (req, res) => {
 
   const sessions = await Session.find({ userId }).sort({ loginTime: -1 });
 
-  res.status(200).json(sessions);
+  res.status(200).json({ sessions });
 });
 
 export const getMySessions = TryCatch(async (req, res) => {
   const userId = req.user._id;
 
   const sessions = await Session.find({ userId }).sort({ loginTime: -1 });
-  res.status(200).json(sessions);
+  res.status(200).json({ sessions });
+});
+
+export const getActiveSessions = TryCatch(async (req, res) => {
+  const sessions = await Session.find({ status: "active" })
+    .sort({
+      loginTime: -1,
+    })
+    .populate("userId", "name email role");
+
+  res.status(200).json({ sessions });
+});
+
+export const getMyActiveSessions = TryCatch(async (req, res) => {
+  const userId = req.user._id;
+
+  const sessions = await Session.find({ userId, status: "active" }).sort({
+    loginTime: -1,
+  });
+
+  res.status(200).json({ sessions });
 });
--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import {
+  getActiveSessions,
   getAllSessions,
+  getMyActiveSessions,
   getMySessions,
   getUserSessions,
 } from "../controllers/sessionController.js";
@@ -10,8 +12,10 @@ import {
 const router = express.Router();
 
 router.get("/my", isAuth, getMySessions);
+router.get("/my/active", isAuth, getMyActiveSessions);
 
 router.get("/", isAuth, isAdmin, getAllSessions);
+router.get("/active", isAuth, isAdmin, getActiveSessions);
 router.get("/user/:userId", isAuth, isAdmin, getUserSessions);
 
 export default router;
