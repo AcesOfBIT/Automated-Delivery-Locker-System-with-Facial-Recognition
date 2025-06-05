dde9ffd590102a17596ba8b67ca5729ef63ec180
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-05 10:08:34 +0530

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,56 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
+import TryCatch from "../utils/TryCatch.js";
 
-export const registerUser = async (req, res) => {
-  try {
-    const { name, email, faceId, phone } = req.body;
+export const registerUser = TryCatch(async (req, res) => {
+  const { name, email, faceId, phone } = req.body;
 
-    let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-    if (user) {
-      return res.status(400).json({
-        message: "Already have account with this email",
-      });
-    }
-    const hashFaceId = await bcrypt.hash(faceId, 10);
-
-    user = await User.create({
-      name,
-      email,
-      phone,
-      faceId: hashFaceId,
+  if (user) {
+    return res.status(400).json({
+      message: "Already have account with this email",
     });
-
-    res.status(201).json({
-        user,
-        message: "User Created"
-    })
-  } catch (error) {
-    console.log(error);
   }
-};
+  const hashFaceId = await bcrypt.hash(faceId, 10);
+
+  user = await User.create({
+    name,
+    email,
+    phone,
+    faceId: hashFaceId,
+  });
+  generateToken(user._id, res);
+  res.status(201).json({
+    user,
+    message: "User Created",
+  });
+});
+
+export const loginUser = TryCatch(async (req, res) => {
+  const { email, faceId } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "No user with this email",
+    });
+  }
+
+  const compareFaceId = await bcrypt.compare(faceId, user.faceId);
+
+  if (!compareFaceId) {
+    return res.status(400).json({
+      message: "Password doesn't match. Please try again",
+    });
+  }
+
+  generateToken(user._id, res);
+
+  res.json({
+    user,
+    message: "Logged in successfully",
+  });
+});
