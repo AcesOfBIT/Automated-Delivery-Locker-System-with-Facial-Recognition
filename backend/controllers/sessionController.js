b375ba0d5a4aff09357e4830e2ca94aa1c1058f3
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-05 22:42:52 +0530

    get user sessions

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -5,3 +5,18 @@ export const getAllSessions = TryCatch(async (req, res) => {
   const sessions = await Session.find().populate("userId", "name email role");
   res.status(200).json(sessions);
 });
+
+export const getUserSessions = TryCatch(async (req, res) => {
+  const { userId } = req.params;
+
+  const sessions = await Session.find({ userId }).sort({ loginTime: -1 });
+
+  res.status(200).json(sessions);
+});
+
+export const getMySessions = TryCatch(async (req, res) => {
+  const userId = req.user._id;
+
+  const sessions = await Session.find({ userId }).sort({ loginTime: -1 });
+  res.status(200).json(sessions);
+});
--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
-import { getAllSessions } from "../controllers/sessionController.js";
+import {
+  getAllSessions,
+  getMySessions,
+  getUserSessions,
+} from "../controllers/sessionController.js";
 
 const router = express.Router();
 
+router.get("/my", isAuth, getMySessions);
+
 router.get("/", isAuth, isAdmin, getAllSessions);
+router.get("/user/:userId", isAuth, isAdmin, getUserSessions);
 
 export default router;
