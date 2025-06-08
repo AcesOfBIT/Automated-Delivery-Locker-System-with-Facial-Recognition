0e67994ca4c664ac52711be72a1a51311a0a967b
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-06-08 23:50:18 +0530

    filter assignment log

--- backend/controllers/assignmentLogController.js
+++ backend/controllers/assignmentLogController.js
@@ -2,9 +2,23 @@ import { AssignmentLog } from "../models/assignmentLogModel.js";
 import TryCatch from "../utils/TryCatch.js";
 
 export const getAssignmentLogs = TryCatch(async (req, res) => {
-  const logs = await AssignmentLog.find()
+  const { source, after, before } = req.query;
+
+  const query = {};
+
+  if (source) {
+    query.source = source;
+  }
+
+  if (after || before) {
+    query.assignedAt = {};
+    if (after) query.assignedAt.$gte = new Date(after);
+    if (before) query.assignedAt.$lte = new Date(before);
+  }
+
+  const logs = await AssignmentLog.find(query)
     .populate("packageId", "trackingId")
-    .populate("lockerId", "lockerId size")
+    .populate("lockerId", "lockerNumber size")
     .populate("assignedBy", "name email role")
     .sort({ assignedAt: -1 });
 
